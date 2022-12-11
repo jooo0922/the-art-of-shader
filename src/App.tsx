60ee5543d5963492e8f0657a079e89fbdd4e2ce9
jooo0922 <2014102458@khu.ac.kr>
2022-12-11 22:38:57 +0900

    Revert "feat: Main 페이지 컴포넌트 추가"
    This reverts commit 3dff13f29c3949fc0ca09861a84bfd27a99dacfb.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import React from "react";
 import "./App.css";
-import Main from "./pages/Main/Main";
-import Work from "./pages/Work/Work";
-import About from "./pages/About/About";
-import { Route, Routes, useLocation } from "react-router-dom";
 
 const App: React.FC = () => {
-  const location = useLocation();
-  return (
-    <div className="App">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/work" element={<Work />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
--- src/pages/Main/Main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Main.module.css";
-
-const Main: React.FC = () => {
-  return <div className={styles.Main}>Main</div>;
-};
-
-export default Main;
