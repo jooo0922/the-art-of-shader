a92e0735288ecf4d0258b090bb4b6721ec9265b9
jooo0922 <2014102458@khu.ac.kr>
2022-12-12 00:32:24 +0900

    feat: About 페이지에 Header 컴포넌트 추가 및 margin 설정

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "../../components/Header/Header";
 import styles from "./About.module.css";
 
 const About: React.FC = () => {
-  return <div className={styles.About}>About</div>;
+  return (
+    <div className={styles.About}>
+      <Header></Header>
+    </div>
+  );
 };
 
 export default About;
