917efe30e7744acd71b7b63cb19b172129b0eabd
jooo0922 <2014102458@khu.ac.kr>
2022-12-11 22:42:05 +0900

    feat: About 페이지 컴포넌트 추가

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./About.module.css";
+
+const About: React.FC = () => {
+  return <div className={styles.About}>About</div>;
+};
+
+export default About;
