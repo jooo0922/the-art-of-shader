627ccb5759007d9a5210751e1b094b8768832f58
jooo0922 <2014102458@khu.ac.kr>
2022-12-12 00:32:46 +0900

    feat: Main 페이지에 Header 컴포넌트 추가 및 margin 설정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "../../components/Header/Header";
 import styles from "./Main.module.css";
 
 const Main: React.FC = () => {
-  return <div className={styles.Main}>Main</div>;
+  return (
+    <div className={styles.Main}>
+      <Header></Header>
+    </div>
+  );
 };
 
 export default Main;
