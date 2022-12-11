4d89484fff539c0b99647becb24884592da1216d
jooo0922 <2014102458@khu.ac.kr>
2022-12-12 01:06:47 +0900

    feat: 페이지에 Footer 컴포넌트 추가

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import styles from "./About.module.css";
 
 const About: React.FC = () => {
   return (
     <div className={styles.About}>
       <Header></Header>
+      <div className={styles.Content}></div>
+      <Footer></Footer>
     </div>
   );
 };
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import styles from "./Main.module.css";
 
 const Main: React.FC = () => {
   return (
     <div className={styles.Main}>
       <Header></Header>
+      <div className={styles.Content}></div>
+      <Footer></Footer>
     </div>
   );
 };
