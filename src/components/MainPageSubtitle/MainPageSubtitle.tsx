0ed6cfb937ad41dd640052de1594146a134f054a
jooo0922 <2014102458@khu.ac.kr>
2022-12-12 18:30:05 +0900

    feat: MainPageSubtitle 컴포넌트 추가

---
+++ src/components/MainPageSubtitle/MainPageSubtitle.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./MainPageSubtitle.module.css";
+
+const MainPageSubtitle: React.FC = () => {
+  return (
+    <div className={styles.MainPageSubtitle}>
+      ARTWORKS WITH GLSL <br />
+      RECREATED BY <br />
+      CHA JOOYEONG
+    </div>
+  );
+};
+
+export default MainPageSubtitle;
