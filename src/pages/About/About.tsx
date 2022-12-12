0d7c9fb73ed938408cf5c709948bc6ec6fb6c7f1
jooo0922 <2014102458@khu.ac.kr>
2022-12-13 00:16:10 +0900

    feat: About 페이지에 하위 컴포넌트들 추가

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,12 +1,17 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import AboutPageTitle from "../../components/AboutPageTitle/AboutPageTitle";
+import AboutPageInfo from "../../components/AboutPageInfo/AboutPageInfo";
 import styles from "./About.module.css";
 
 const About = () => {
   return (
     <div className={styles.About}>
       <Header></Header>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <AboutPageTitle></AboutPageTitle>
+        <AboutPageInfo></AboutPageInfo>
+      </div>
       <Footer></Footer>
     </div>
   );
