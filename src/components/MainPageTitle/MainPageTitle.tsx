45ead707edfa51d0795d2de0f4e7670b2b486f4d
jooo0922 <2014102458@khu.ac.kr>
2022-12-12 18:30:18 +0900

    feat: MainPageTitle 컴포넌트 추가

---
+++ src/components/MainPageTitle/MainPageTitle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./MainPageTitle.module.css";
+
+const MainPageTitle: React.FC = () => {
+  return (
+    <div className={styles.MainPageTitle}>
+      <div>THE ART</div>
+      <div>OF SHADER</div>
+    </div>
+  );
+};
+
+export default MainPageTitle;
