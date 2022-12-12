55cd6d9e270b8adbd1816f7736f83d88c8b69535
jooo0922 <2014102458@khu.ac.kr>
2022-12-12 20:57:18 +0900

    feat: Main 페이지에 Thumbnail 컴포넌트 리스트 추가

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,6 +2,8 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import MainPageTitle from "../../components/MainPageTitle/MainPageTitle";
 import MainPageSubtitle from "../../components/MainPageSubtitle/MainPageSubtitle";
+import Thumbnail from "../../components/Thumbnail/Thumbnail";
+import THUMBNAIL_DATA from "./thumbnailData";
 import styles from "./Main.module.css";
 
 const Main = () => {
@@ -13,7 +15,16 @@ const Main = () => {
           <MainPageTitle></MainPageTitle>
           <MainPageSubtitle></MainPageSubtitle>
         </div>
-        <div className={styles.thumbnailContainer}></div>
+        <div className={styles.thumbnailContainer}>
+          {THUMBNAIL_DATA.map((data, index) => (
+            <Thumbnail
+              title={data.title}
+              imageUrl={data.imageUrl}
+              pathName={data.pathName}
+              key={index}
+            />
+          ))}
+        </div>
       </div>
       <Footer></Footer>
     </div>
