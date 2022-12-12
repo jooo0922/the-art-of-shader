2c3d0eefd6df70392154a60690a6d72df116473a
jooo0922 <2014102458@khu.ac.kr>
2022-12-12 18:30:39 +0900

    feat: Main 페이지에 타이틀 컴포넌트들 추가

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import MainPageTitle from "../../components/MainPageTitle/MainPageTitle";
+import MainPageSubtitle from "../../components/MainPageSubtitle/MainPageSubtitle";
 import styles from "./Main.module.css";
 
 const Main: React.FC = () => {
   return (
     <div className={styles.Main}>
       <Header></Header>
-      <div className={styles.Content}></div>
+      <div className={styles.content}>
+        <div className={styles.titleContainer}>
+          <MainPageTitle></MainPageTitle>
+          <MainPageSubtitle></MainPageSubtitle>
+        </div>
+        <div className={styles.thumbnailContainer}></div>
+      </div>
       <Footer></Footer>
     </div>
   );
