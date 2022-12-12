47bac9d68aead6b2d9db26accfd59e8ab15f2723
jooo0922 <2014102458@khu.ac.kr>
2022-12-13 00:15:51 +0900

    feat: AboutPageTitle 컴포넌트 추가

---
+++ src/components/AboutPageTitle/AboutPageTitle.tsx
@@ -0,0 +1,13 @@
+import styles from "./AboutPageTitle.module.css";
+
+const AboutPageTitle = () => {
+  return (
+    <div className={styles.AboutPageTitle}>
+      THE <br />
+      ART OF <br />
+      SHADER
+    </div>
+  );
+};
+
+export default AboutPageTitle;
