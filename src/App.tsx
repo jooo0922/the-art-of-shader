3d7aae075d2895cc8c9613556fa280f5719d8d29
jooo0922 <2014102458@khu.ac.kr>
2022-12-12 20:58:28 +0900

    refactor: <Route> 컴포넌트 직접 추가 -> useRoutes() 호출로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import { useRoutes } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/Main/Main";
 import Work from "./pages/Work/Work";
 import About from "./pages/About/About";
-import { Route, Routes, useLocation } from "react-router-dom";
+import THUMBNAIL_DATA from "./pages/Main/thumbnailData";
 
-const App: React.FC = () => {
-  const location = useLocation();
-  return (
-    <div className="App">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/work" element={<Work />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-    </div>
-  );
+const App = () => {
+  const workPageRouteObjects = THUMBNAIL_DATA.map((data) => {
+    return { path: data.pathName, element: <Work /> };
+  });
+  const routes = useRoutes([
+    { path: "/", element: <Main /> },
+    { path: "/about", element: <About /> },
+    ...workPageRouteObjects,
+  ]);
+
+  return <div className="App">{routes}</div>;
 };
 
 export default App;
