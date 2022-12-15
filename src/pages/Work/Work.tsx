7601440f42634ff91f5043498dea6d3b951301fd
jooo0922 <2014102458@khu.ac.kr>
2022-12-16 01:30:42 +0900

    feat: Work 페이지에서 WebGL 실행 로직 구현

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import WebGLManager from "../../webgl/WebGLManager";
 import styles from "./Work.module.css";
 
 const Work = () => {
-  return <div className={styles.Work}>Work</div>;
+  const canvasRef = useRef(null);
+  const location = useLocation();
+  useEffect(() => {
+    async function initWebGL(): Promise<void> {
+      const canvas = canvasRef.current;
+      const webglManager = new WebGLManager(canvas);
+      webglManager.setPathName(location.pathname);
+      await webglManager.init();
+    }
+    initWebGL();
+  });
+
+  return (
+    <div className={styles.Work}>
+      <canvas ref={canvasRef} className={styles.webglCanvas}></canvas>
+    </div>
+  );
 };
 
 export default Work;
