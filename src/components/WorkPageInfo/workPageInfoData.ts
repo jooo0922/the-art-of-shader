9294436cb8bb3e93345cda7b55929f5964f4279a
jooo0922 <2014102458@khu.ac.kr>
2022-12-17 22:12:52 +0900

    feat: WorkPageInfo 컴포넌트에서 사용할 데이터 객체 추가

---
+++ src/components/WorkPageInfo/workPageInfoData.ts
@@ -0,0 +1,71 @@
+import { WorkPageInfoDataType } from "../../types/WorkPageInfoDataType";
+
+const WORK_PAGE_INFO_DATA: WorkPageInfoDataType[] = [
+  {
+    title: "BURNING BALL",
+    date: "2022.09.12",
+    referenceUrl: "https://dribbble.com/shots/379265-Fireball",
+    pathName: "/burning-ball",
+  },
+  {
+    title: "FORCE FIELD",
+    date: "2022.10.08",
+    referenceUrl:
+      "https://harrypotter.fandom.com/wiki/Forum:New_Hogwarts_for_DH",
+    pathName: "/force-field",
+  },
+  {
+    title: "MAGIC CIRCLE",
+    date: "2022.11.27",
+    referenceUrl:
+      "https://www.deviantart.com/vuduc/art/Game-Fx-Health-Buff-Unity-3D-624876602",
+    pathName: "/magic-circle",
+  },
+  {
+    title: "VORTEX",
+    date: "2022.11.13",
+    referenceUrl: "https://www.pinterest.co.kr/pin/1135821968497627646",
+    pathName: "/vortex",
+  },
+  {
+    title: "TERRAIN",
+    date: "2022.10.23",
+    referenceUrl: "https://www.pinterest.co.kr/pin/1135821968496299749",
+    pathName: "/terrain",
+  },
+  {
+    title: "SHIELD",
+    date: "2022.12.04",
+    referenceUrl: "https://www.youtube.com/watch?app=desktop&v=i2w0p_mV8_g",
+    pathName: "/shield",
+  },
+  {
+    title: "TYPO WARP",
+    date: "2022.10.29",
+    referenceUrl:
+      "https://nicholasmacdonald.co.uk/filter/animation/Kinetic-Typography",
+    pathName: "/typo-warp",
+  },
+  {
+    title: "WATER BLOB",
+    date: "2022.09.17",
+    referenceUrl:
+      "https://giphy.com/gifs/trippy-abstract-pi-slices-QsOUYFOT0Ape5SdPFo",
+    pathName: "/water-blob",
+  },
+  {
+    title: "GRAIN STATUE",
+    date: "2022.11.07",
+    referenceUrl:
+      "https://www.behance.net/gallery/106782599/Risograph-Grain-Effect-for-Photoshop",
+    pathName: "/grain-statue",
+  },
+  {
+    title: "GALAXY",
+    date: "2022.09.24",
+    referenceUrl: "https://www.youtube.com/watch?v=o_bEveIFfoM",
+    pathName: "/galaxy",
+  },
+];
+
+export default WORK_PAGE_INFO_DATA;
