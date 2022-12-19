8e3cf7ca32967b3ad87625aede3001c1dc86afd2
jooo0922 <2014102458@khu.ac.kr>
2022-12-19 23:42:49 +0900

    design: Header hiding 인터랙션 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,32 @@
+import { useState, useEffect } from "react";
 import Logo from "../Logo/Logo";
 import AboutBtn from "../AboutBtn/AboutBtn";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [hide, setHide] = useState(false);
+
+  useEffect(() => {
+    let prevScrollY = window.scrollY;
+    const onScroll = (e: Event) => {
+      const curScrollY = window.scrollY;
+      if (curScrollY === 0 || curScrollY < prevScrollY) {
+        setHide(false); // scroll top || scroll up
+      } else {
+        setHide(true); // scroll down
+      }
+      prevScrollY = curScrollY;
+    };
+
+    window.addEventListener("scroll", onScroll, false);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll, false);
+    };
+  }, []);
+
   return (
-    <header className={styles.Header}>
+    <header className={`${styles.Header} ${hide ? styles.hide : null}`}>
       <Logo></Logo>
       <AboutBtn></AboutBtn>
     </header>
