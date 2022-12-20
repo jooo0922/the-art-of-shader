f688b871fc2d4ac5acdade36977aec91ce12f2ac
jooo0922 <2014102458@khu.ac.kr>
2022-12-20 23:10:01 +0900

    feat: Thumbnail 컴포넌트 fadeup 모션 추가

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import styles from "./Thumbnail.module.css";
 import { ThumbnailDataType } from "../../types/ThumbnailDataType";
 
 const Thumbnail = ({ title, imageUrl, pathName }: ThumbnailDataType) => {
   return (
-    <div className={styles.Thumbnail}>
+    <motion.div
+      className={styles.Thumbnail}
+      initial={{ y: 50, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      viewport={{ once: true, amount: 0.4 }}
+      transition={{ duration: 1, ease: [0.25, 0.46, 0.45, 0.94] }}
+    >
       <Link to={pathName} className={styles.link}>
         <img src={imageUrl} alt="thumbnail" />
         <div className={styles.title}>{title}</div>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
